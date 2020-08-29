eb4c780a003c8f0b6d8c59157c906d4ba6af8290
GlhrmeSoares <64655650+GlhrmeSoares@users.noreply.github.com>
2020-08-29 12:34:02 -0300

    Depois de uma baita refatorada
    Optimizei o código em geral, diminuiu consideravelmente a quantidade de linhas e adicionei a funcionalidade para rolar mais de um dado por vez.

--- Dices/Dados.py
+++ Dices/Dados.py
@@ -1,44 +1,6 @@
 import random
 
 
-class Rolagem:
-
-    @staticmethod
-    def dado4():
-        dado = random.randrange(1, 5)
-        return dado
-
-    @staticmethod
-    def dado6():
-        dado = random.randrange(1, 7)
-        return dado
-
-    @staticmethod
-    def dado8():
-        dado = random.randrange(1, 9)
-        return dado
-
-    @staticmethod
-    def dado10():
-        dado = random.randrange(1, 11)
-        return dado
-
-    @staticmethod
-    def dado12():
-        dado = random.randrange(1, 13)
-        return dado
-
-    @staticmethod
-    def dado20():
-        dado = random.randrange(1, 21)
-        return dado
-
-    @staticmethod
-    def dado100():
-        dado = random.randrange(1, 101)
-        return dado
-
-
 class Visualizador:
 
     def __init__(self):
@@ -47,8 +9,10 @@ class Visualizador:
     def __eq__(self, other):
         return self.valores[-1] != other.numero_final or self.valores == other.numero_final
 
-    def mostra_hist(self):
-        print(self.valores[:])
+    @staticmethod
+    def dado(quantos_lados):
+        dado = random.randrange(1, quantos_lados + 1)
+        return dado
 
     def arquivo_hist(self, valor):
         self.valores.append(valor)
@@ -62,150 +26,32 @@ class Visualizador:
             i += 1
             return antirepeticao
 
-    def tem_numero_repetido(self, numero_final, dice):
-        numero = Rolagem()
+    def tem_numero_repetido(self, numero_final, quantos_lados):
         while self.valores[-1] == numero_final:
-            if dice == 'd4':
-                numero_final = numero.dado4()
-                return numero_final
-            if dice == 'd6':
-                numero_final = numero.dado6()
-                return numero_final
 
-            if dice == 'd8':
-                numero_final = numero.dado8()
-                return numero_final
-
-            if dice == 'd10':
-                numero_final = numero.dado10()
-                return numero_final
-
-            if dice == 'd12':
-                numero_final = numero.dado12()
-                return numero_final
-
-            if dice == 'd20':
-                numero_final = numero.dado20()
-                return numero_final
-
-            if dice == 'd100':
-                numero_final = numero.dado100()
-                return numero_final
-
-    def refatorado_com_e_sem_repeticao(self, numero_final, dice):
-        if dice == 'd4':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd4')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-        if dice == 'd6':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd6')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-        if dice == 'd8':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd8')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-
-        if dice == 'd10':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd10')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-
-        if dice == 'd12':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd12')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-
-        if dice == 'd20':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd20')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
-
-        if dice == 'd100':
-            if self.valores[-1] != numero_final:
-                self.sem_numero_repetido(numero_final)
-                return numero_final
-
-            else:
-                numero_refatorado = self.tem_numero_repetido(numero_final, 'd100')
-                self.arquivo_hist(numero_refatorado)
-                return numero_refatorado
+            numero_final = self.dado(quantos_lados)
+            return numero_final
 
     def mostra_para_o_usuario(self):
+
         while True:
-            numero = Rolagem()
-
             dice = input('Qual dado vamos rodar??')
-
-            if dice == 'd4':
-
-                numero_final = numero.dado4()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd4')
-                self.mostra_hist()
+            lado = dice.split(sep='d')
+            quantos_lados, vezes, n = lado[-1], lado[0], 1
+            quantos_lados, vezes = int(quantos_lados), int(vezes)
+            while n <= vezes:
+                numero_final = self.dado(quantos_lados)
+                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, quantos_lados + 1)
                 print(refatorado)
+                n += 1
 
-            if dice == 'd6':
-                numero_final = numero.dado6()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd6')
-                self.mostra_hist()
-                print(refatorado)
+    def refatorado_com_e_sem_repeticao(self, numero_final, quantos_lados):
 
-            if dice == 'd8':
-                numero_final = numero.dado8()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd8')
-                self.mostra_hist()
-                print(refatorado)
+        if self.valores[-1] != numero_final:
+            self.sem_numero_repetido(numero_final)
+            return numero_final
 
-            if dice == 'd10':
-                numero_final = numero.dado10()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd10')
-                self.mostra_hist()
-                print(refatorado)
-
-            if dice == 'd12':
-                numero_final = numero.dado12()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd12')
-                self.mostra_hist()
-                print(refatorado)
-
-            if dice == 'd20':
-                numero_final = numero.dado20()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd20')
-                self.mostra_hist()
-                print(refatorado)
-
-            if dice == 'd100':
-                numero_final = numero.dado100()
-                refatorado = self.refatorado_com_e_sem_repeticao(numero_final, 'd100')
-                self.mostra_hist()
-                print(refatorado)
+        else:
+            numero_refatorado = self.tem_numero_repetido(numero_final, quantos_lados)
+            self.arquivo_hist(numero_refatorado)
+            return numero_refatorado
